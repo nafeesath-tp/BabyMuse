363db7618af1df994264dc9e166ced7b1586dea3
Nafeesath T P <tpnafeesath90@gmail.com>
2025-08-26 12:25:08 +0530

    fixed in settings.py

--- babymuse/settings.py
+++ babymuse/settings.py
@@ -147,7 +147,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+  BASE_DIR /  "static",
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
